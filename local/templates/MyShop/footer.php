1bb83ce19467e638cc7b3c67f1f18ed94297c792
Avangardio <12lol34lol56lol@gmail.com>
2023-05-10 16:37:18 +0300

    жэс

--- local/templates/MyShop/footer.php
+++ local/templates/MyShop/footer.php
@@ -1,29 +1,11 @@
 <?php
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
-use Bitrix\Main\Page\Asset;
-
-$asset = Asset::getInstance();
-$asset -> addCss(SITE_TEMPLATE_PATH . "/styles.css")
-
 ?>
 
-<!DOCTYPE html>
-<html>
-  <head>
-
-    <?php $APPLICATION -> ShowHead()?>
-
-    <title><?php $APPLICATION->ShowTitle()?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-  </head>
-  <main>
-    <!-- Хидер -->
-    <header>
-      <h1>Моя компания</h1>
-      <nav>
-        <a href="#">Главная</a>
-        <a href="#">Новости</a>
-        <a href="#">Данные</a>
-      </nav>
-    </header>
+        <footer>
+            <p>© 2023 Моя компания</p>
+            <p>Адрес: ул. Примерная, 123</p>
+            <p>Телефон: +7 (999) 123-45-67</p>
+        </footer>
+    </main>
+</html>
