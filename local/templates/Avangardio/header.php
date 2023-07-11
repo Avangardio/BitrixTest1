f1574adb954284b41f114bd40c9d8b28d6b60081
Avangardio <12lol34lol56lol@gmail.com>
2023-07-11 15:31:59 +0300

    1

--- local/templates/Avangardio/header.php
+++ local/templates/Avangardio/header.php
@@ -1,24 +1,12 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-IncludeTemplateLangFile(__FILE__);
-?>
-    <html>
-    <head>
-        <?$APPLICATION->ShowHead();?>
-        <title><?$APPLICATION->ShowTitle()?></title>
-    </head>
-
-<body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" bgcolor="#FFFFFF">
-
-<?$APPLICATION->ShowPanel()?>
-
-<?if($USER->IsAdmin()):?>
-
-    <div style="border:red solid 1px">
-        <form action="/bitrix/admin/site_edit.php" method="GET">
-            <input type="hidden" name="LID" value="<?=SITE_ID?>" />
-            <p><font style="color:red"><?echo GetMessage("DEF_TEMPLATE_NF")?> </font></p>
-            <input type="submit" name="set_template" value="<?echo GetMessage("DEF_TEMPLATE_NF_SET")?>" />
-        </form>
-    </div>
-
-<?endif?>
+?><!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=<?echo LANG_CHARSET;?>">
+    <?$APPLICATION->ShowMeta("keywords");?>
+    <?$APPLICATION->ShowMeta("description");?>
+    <title><?$APPLICATION->ShowTitle()?></title>
+    <?$APPLICATION->ShowHead()?>
+</head>
+<body>
+<?$APPLICATION->ShowPanel();?>
