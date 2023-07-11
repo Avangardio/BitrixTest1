a02a35b619cd3789f2dd69f0ae6fb76a3fe251e5
Avangardio <12lol34lol56lol@gmail.com>
2023-07-11 15:10:06 +0300

    1

--- local/templates/Avangardio/footer.php
+++ local/templates/Avangardio/footer.php
@@ -1,30 +1,27 @@
-<?php
-if (! defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
-?>
-</div>
-<footer class="py-5 bg-dark">
-    <div class="container text-center mb-4">
-        <div class="row">
-            <div class="col-md-4">
-                <a href="index.html" class="text-white">На главную</a>
-            </div>
-            <div class="col-md-4">
-                <a href="faq.html" class="text-white">Вопросы и ответы</a>
-            </div>
-            <div class="col-md-4">
-                <a href="catalog.html" class="text-white">Каталог</a>
-            </div>
+<footer class="footer">
+    <div class="footer__content">
+        <div class="footer__logo">
+            <a href="/">
+                <img src="<?=SITE_TEMPLATE_PATH?>/img/logo.svg" alt="Логотип">
+            </a>
+        </div>
+        <div class="footer__menu">
+            <?$APPLICATION->IncludeComponent(
+                "bitrix:menu",
+                "bottom",
+                Array(
+                    "ROOT_MENU_TYPE" => "bottom",
+                    "MAX_LEVEL" => "1",
+                    "USE_EXT" => "N"
+                )
+            );?>
+        </div>
+        <div class="footer__contacts">
+            <p>Телефон: +7 (495) 123-45-67</p>
+            <p>Email: info@example.com</p>
         </div>
     </div>
-    <div class="container">
-        <p class="m-0 text-center text-white">
-            Copyright &copy; Обжорозаврик 2020.
-            Создано для проекта <a href="https://bitrixcasts.ru" rel="nofollow" target="_blank">BitrixCasts</a>.
-            Автор <a href="https://mvsvolkov.ru" rel="nofollow" target="_blank">Волков Михаил</a> <i class="fas fa-heart text-danger"></i>
-        </p>
+    <div class="footer__bottom">
+        <p>&copy; <?=date('Y')?> Название компании</p>
     </div>
 </footer>
-</body>
-
-</html>
-
--- local/templates/Avangardio/header.php
+++ local/templates/Avangardio/header.php
@@ -1,28 +1,28 @@
-<?php
-
-if (! defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
-
-use Bitrix\Main\Page\Asset;
-
-$asset = Asset::getInstance();
-?>
 <!DOCTYPE html>
 <html lang="ru">
-
 <head>
-    <link href="<?=SITE_TEMPLATE_PATH?>/bootstrap/css/bootstrap.css" rel="stylesheet">
-    <script src="<?=SITE_TEMPLATE_PATH?>/assets/js/bootstrap.js"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <?php $APPLICATION->ShowHead()?>
-
-    <title><?php $APPLICATION->ShowTitle()?></title>
-
-    <link rel="apple-touch-icon" sizes="180x180" href="<?=SITE_TEMPLATE_PATH?>/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?=SITE_TEMPLATE_PATH?>/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?=SITE_TEMPLATE_PATH?>/favicon/favicon-16x16.png">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?$APPLICATION->ShowTitle()?></title>
+    <?$APPLICATION->ShowHead()?>
+    <link rel="stylesheet" href="<?=SITE_TEMPLATE_PATH?>/css/style.css">
 </head>
-
 <body>
-<?php $APPLICATION->ShowPanel()?>
-<div class="main-wrapper" id="app">
-
+<header class="header">
+    <div class="header__logo">
+        <a href="/">
+            <img src="<?=SITE_TEMPLATE_PATH?>/img/logo.svg" alt="Логотип">
+        </a>
+    </div>
+    <nav class="header__menu">
+        <?$APPLICATION->IncludeComponent(
+            "bitrix:menu",
+            "top",
+            Array(
+                "ROOT_MENU_TYPE" => "top",
+                "MAX_LEVEL" => "1",
+                "USE_EXT" => "N"
+            )
+        );?>
+    </nav>
+</header>
