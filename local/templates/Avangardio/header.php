39ae2f76d63ab1d7235cf655ad491517c4d56965
Avangardio <12lol34lol56lol@gmail.com>
2023-07-11 15:51:26 +0300

    1

--- local/templates/Avangardio/header.php
+++ local/templates/Avangardio/header.php
@@ -1,12 +1,24 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-?><!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<?php
+
+if (! defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+
+use Bitrix\Main\Page\Asset;
+
+$asset = Asset::getInstance();
+$asset->addCss(SITE_TEMPLATE_PATH . '/bootstrap/css/bootstrap.css');
+$asset->addJs(SITE_TEMPLATE_PATH . '/bootstrap/js/bootstrap.js');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=<?echo LANG_CHARSET;?>">
-    <?$APPLICATION->ShowMeta("keywords");?>
-    <?$APPLICATION->ShowMeta("description");?>
-    <title><?$APPLICATION->ShowTitle()?></title>
-    <?$APPLICATION->ShowHead()?>
-</head>
-<body>
-<?$APPLICATION->ShowPanel();?>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=<?echo LANG_CHARSET;?>">
+        <?$APPLICATION->ShowMeta("keywords");?>
+        <?$APPLICATION->ShowMeta("description");?>
+        <title><?$APPLICATION->ShowTitle()?></title>
+        <?$APPLICATION->ShowHead()?>
+    </head>
+    <body>
+        <?$APPLICATION->ShowPanel();?>
+        <header>
+            <div class="w-100 blue"> </div>
+        </header>
