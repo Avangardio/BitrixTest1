3d74be8b6fc811461d49d0aa2caeec4ba93ae20f
Avangardio <12lol34lol56lol@gmail.com>
2023-07-13 23:19:57 +0300

    1

--- infoblocks/ajax/addTodo.php
+++ infoblocks/ajax/addTodo.php
@@ -1,25 +1,32 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . "/bitrix/modules/main/include/prolog_before.php");
 CModule::IncludeModule("iblock");
-    $props = array(
-        "PROPERTY_1" => "Значение свойства 1",
-        "PROPERTY_2" => "Значение свойства 2",
-        "TASK" => $_POST['taskName']);
-        // добавьте свойства, которые нужны для вашего элемента
-    $fields = array(
-        "IBLOCK_ID" => 5,
-        "IBLOCK_CODE" => "content",
-        "NAME" => "Название элемента",
-        "ACTIVE" => "Y",
-        "PROPERTY_VALUES" => $props, // массив свойств, созданный на предыдущем шаге
-    );
-        $el = new CIBlockElement;
-        $element_id = $el->Add($fields);
 
-        if ($element_id) {
-            echo "Новый элемент успешно добавлен с ID " . $element_id;
-            header("Content-type: application/json; charset=utf-8");
-            echo ('ok');
-        } else {
-            echo "Ошибка при добавлении нового элемента: " . $el->LAST_ERROR;
-        }
+// создаем массив свойств для нового элемента
+$props = array(
+    "PROPERTY_1" => "Значение свойства 1",
+    "PROPERTY_2" => "Значение свойства 2",
+    "TASK" => $_POST['taskName']
+    // добавьте свойства, которые нужны для вашего элемента
+);
+
+// создаем массив с данными для нового элемента
+$fields = array(
+    "IBLOCK_ID" => 5,
+    "IBLOCK_CODE" => "content",
+    "NAME" => "Название элемента",
+    "ACTIVE" => "Y",
+    "PROPERTY_VALUES" => $props, // массив свойств, созданный на предыдущем шаге
+);
+
+// добавляем новый элемент в инфоблок
+$el = new CIBlockElement;
+$element_id = $el->Add($fields);
+
+// проверяем результат и выводим сообщение
+if ($element_id) {
+    echo "Новый элемент успешно добавлен с ID " . $element_id;
+} else {
+    echo "Ошибка при добавлении нового элемента: " . $el->LAST_ERROR;
+}
+?>
