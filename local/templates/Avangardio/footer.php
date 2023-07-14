c2d2381900ea01723ef2133d02baf1270c6d21af
Avangardio <12lol34lol56lol@gmail.com>
2023-07-14 15:26:55 +0300

    1

--- local/templates/Avangardio/footer.php
+++ local/templates/Avangardio/footer.php
@@ -1,4 +1,4 @@
-<footer class="w-100 bg-secondary h-25" style="max-height: 100px;">
+<footer class="w-100 bg-secondary h-25 bottom-0" style="max-height: 100px;">
     <div> </div>
 </footer>
 </body>
